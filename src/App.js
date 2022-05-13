bb2b08c2ce691e34f98176d51f46605f99e1ad81
IvanovAndrew1998 <ivanovanreyka@gmail.com>
2022-05-13 20:32:51 +0300

    Fetching, Sorting, Filtering. I kastil v pridachu

--- src/App.js
+++ src/App.js
@@ -1,59 +1,68 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import Postlist from './components/PostList/Postlist.module';
-import ModalWindow from './components/ModalWindow/ModalWindow';
 import axios from 'axios';
-import useFetching from './hooks/useFeching';
+import { useEffect, useState } from 'react';
+import './App.css';
+import ModalWindow from './components/ModalWindow/ModalWindow';
+import Postlist from './components/PostList/Postlist.module';
 import Sidebar from './components/Sidebar/Sidebar';
+import useFetching from './hooks/useFeching';
 
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [modalvis, setModalVis] = useState(false);
-  const [filtered, setFilter] = useState("unfiltred");
-  const [isPostFecting, fetchPostO] = useFetching(fetchPosts);
+  const [isPostFecting, fetchFunc] = useFetching(fetchPosts);
+
+  const [filtredPosts, setFiltredPosts] = useState([]);
+  const [sorted, setSorted] = useState("straight");
   const [bukved, setBukved] = useState();
 
   useEffect(
     () => {
-      fetchPostO();
-    }, []
+            fetchFunc(() => fetchPosts());
+            setFiltredPosts(posts);
+          }, 
+    []
   );
 
   useEffect(
-    // () => setPosts(sortPosts(filterPosts(posts, bukved), filtered)),
-    // () => console.log(sortPosts(filterPosts(posts, bukved), filtered)),
-    [filtered, bukved]
+    () => filterPosts()
+    ,
+    [sorted, bukved]
   );
 
 
   async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    
     function transform (post_elem) {
       return {heading: post_elem.title, 
               data: post_elem.body}
     }
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
     setPosts(response.data.map(transform));
+    setFiltredPosts(response.data.map(transform));
   }
+
 
   function addPost(heading, body) {
     setPosts([{heading: heading, data: body}, ...posts]);
   }
 
-  function sortPosts(posts, filterarg) {
-    console.log(posts);
-    switch (filterarg) {
+  
+  function filterPosts() {
+    const filtred_posts = posts.filter( 
+      post => post.heading.includes(bukved)
+    );
+
+    let processed_post = Object;
+    switch (sorted) {
       case "straight":
-        return [...posts].sort((a, b) => a.heading.localeCompare(b.heading))
+        processed_post = filtred_posts.sort((a, b) => a.heading.localeCompare(b.heading))
         break;
       case 'reversed':
-        return [...posts].sort((a, b) => b.heading.localeCompare(a.heading))
+        processed_post = filtred_posts.sort((a, b) => b.heading.localeCompare(a.heading))
       }
-  }
-
-  function filterPosts(posts, filterarg) {
-    return [...posts].filter(post => post.heading.includes(filterarg), posts);
-  }
+      setFiltredPosts(processed_post);
+  };
 
 
 
@@ -64,8 +73,8 @@ function App() {
           <button onClick={() => setModalVis(true) } >Добавить</button>
         </header>
         <div className='content'>
-          <Sidebar changeFiltred={setFilter} setBukved={setBukved}></Sidebar>
-          <Postlist posts={posts} isFetching={isPostFecting}/>
+          <Sidebar changeSorted={setSorted} setBukved={setBukved}></Sidebar>
+          <Postlist posts={filtredPosts} isFetching={isPostFecting}/>
         </div>
         
         <ModalWindow visible={modalvis} setVisible={setModalVis} addPost={addPost} />
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Sidebar.module.css';
 
-export default function Sidebar({changeFiltred, setBukved}) {
+export default function Sidebar({changeSorted, setBukved}) {
   return (
     <div className={styles.sidebar}>
-      <select name='rot_ebal' onChange={(e) => {changeFiltred(e.target.value)}}>
+      <select name='rot_ebal' onChange={(e) => {changeSorted(e.target.value)}}>
           <option value="straight">По возрастанию</option>
           <option value="reversed">По убыванию</option>
       </select>
